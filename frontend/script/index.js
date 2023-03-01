d359af2dc6295b5d8c26dcae754f7aabea707c39
Denis-92 <d.mozenic@gmail.com>
2023-03-01 14:13:57 +0100

    aggiunto filtro in index

--- frontend/script/index.js
+++ frontend/script/index.js
@@ -1,6 +1,37 @@
 console.log("---- JS - OK");
 
-elencoPizze();
+const URLParams = new URLSearchParams(window.location.search);
+const filtro = URLParams.get("filtro");
+
+
+
+if (filtro == null)
+    elencoPizze();
+else {
+
+    axios.get(`http://localhost:8080/pizze?filtro=${filtro}`).then((result) => {
+        console.log("test axios.get OK", result);
+        document.querySelector("#table_elenco_pizze").innerHTML = '';
+        result.data.forEach(pizza => {
+            console.log("vedi pizza: ", pizza);
+            document.querySelector("#table_elenco_pizze").innerHTML +=
+                `
+            <tr>
+                <td>
+                    <a href="./show.html?id=${pizza.id}">${pizza.id}</a>
+                </td>
+                <td>${pizza.nome}</td>
+                <td>${pizza.descrizione}</td>
+                <td>${pizza.prezzo} €</td>
+            </tr>
+            `
+        });
+    }).catch((result) => {
+        console.log("Errore row 11 show.js", result);
+        alert("Errore");
+    })
+
+}
 
 function elencoPizze() {
     axios.get('http://localhost:8080/pizze').then((result) => {
